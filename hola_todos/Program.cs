e9cc19cc107b583c9bddff828315f5bfb31f1c51
Kenneth <kalacuna@uamv.edu.ni>
2024-10-11 08:10:33 -0600

    first commit

---
+++ hola_todos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace hola_todos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
